efeed0f094fdd487168a9238a025d0d57df44eff
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-10-08 22:50:06 +0200

    communicate two java client using JMS Queue mode

--- src/main/java/jmsChat/Consumer.java
+++ src/main/java/jmsChat/Consumer.java
@@ -1,5 +1,51 @@
 package jmsChat;
 
-public class Consumer {
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws JMSException {
+		
+		ConnectionFactory cf = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		
+		Connection connection = cf.createConnection();
+		
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination destination = session.createQueue("lyes.queue");
+		 
+		MessageConsumer messageConsumer = session.createConsumer(destination);
+		 
+		messageConsumer.setMessageListener(new MessageListener() {
+
+		@Override
+		public void onMessage(Message message) {
+			
+			if(message instanceof TextMessage) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+						System.out.println(" Message reçu "+ textMessage.getText());
+					} catch (JMSException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			 
+		 });
+		
+	}
 }
--- src/main/java/jmsChat/Producer.java
+++ src/main/java/jmsChat/Producer.java
@@ -1,5 +1,32 @@
 package jmsChat;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class Producer {
+	
+	public static void main(String[] args) throws JMSException {
+		ConnectionFactory cf  = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		Connection connection = cf.createConnection();
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue("lyes.queue");
+		MessageProducer messageProducer = session.createProducer(destination);
+		messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		TextMessage textMessage = session.createTextMessage();
+		textMessage.setText("Hello lyes !!");
+		messageProducer.send(textMessage);
+		System.out.println("Envoi du message");
+		session.close();
+		connection.close();
+	}
 
 }
